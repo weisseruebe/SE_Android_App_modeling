e98b6cd006c89d96626caa28d1b37c48c938f876
David Nöggerath <dnoeggerath@online.de>
2012-02-09 15:00:05 +0100

    doppelte einträge in der *.uml entfernt

--- src_gen/src/test/model/EditPoiActivity.java
+++ src_gen/src/test/model/EditPoiActivity.java
@@ -1,16 +1,9 @@
 package test/model;
 
-
-
 public class EditPoiActivity extends Activity  {
 
 	public Button btnSavePos;
 	
-	public Button testBTN;
-	
-	public Integer testProperty1;
-	
-
 
 	/** Called when the activity is first created. */
 	@Override
@@ -18,51 +11,27 @@ public class EditPoiActivity extends Activity  {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editPoiActivity);
 		
-		org.eclipse.emf.ecore.impl.DynamicEObjectImpl@9af2a2 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@afa268 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false))
+		org.eclipse.emf.ecore.impl.DynamicEObjectImpl@1563422 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@78ba68 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false))
 		Qualified Name:	test/model::btnSavePos
 		Name:			btnSavePos
-		
-		
 		
 		btnSavePos.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@9af2a2 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@afa268 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiName;
-				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@9af2a2 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@afa268 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiDesc;
+				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@1563422 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@78ba68 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiName;
+				RecordRouteMapActivity recordRouteMapActivity;
+				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@1563422 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@78ba68 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiDesc;
+				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@1563422 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@78ba68 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiName;
+				org.eclipse.emf.ecore.impl.DynamicEObjectImpl@1563422 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@78ba68 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false)) poiDesc;
 				Integer testPropertyBtn1;
 				// TODO: implement stuff			
 			}
 		});
 		
-		org.eclipse.emf.ecore.impl.DynamicEObjectImpl@9af2a2 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@afa268 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false))
-		Qualified Name:	test/model::testBTN
-		Name:			testBTN
-		
-		
-		
-		testBTN.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO: implement stuff			
-			}
-		});
-		
-		org.eclipse.emf.ecore.impl.DynamicEObjectImpl@9af2a2 (eClass: org.eclipse.emf.ecore.impl.EClassImpl@afa268 (name: OclInvalid_Class) (instanceClassName: null) (abstract: false, interface: false))
-		Qualified Name:	UMLPrimitiveTypes::Integer
-		Name:			Integer
-		
-		
-		
-		
 	}
 
 	
-	public void testOperation1() {
-		// TODO: Add your code here
-		
-	}
-	
-	private void testOperation2() {
+	private void autoFillWidgets() {
 		// TODO: Add your code here
 		
 	}
